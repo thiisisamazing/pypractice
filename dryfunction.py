c88d094b9ac2e9319d17a53cc0155065691e2e4c
thiisisamazing <34226777+thiisisamazing@users.noreply.github.com>
2018-10-26 08:00:27 -0500

    Create dryfunction.py

---
+++ dryfunction.py
@@ -0,0 +1,6 @@
+def word(monty):
+    print (monty+"rocks!")
+    
+for monty in ("spam ","eggs "):
+    word(monty)
+#rather than writing a bunch of garbage out, we can just use a for loop. pretty groovy.
