e4515f88b3814ebcd12b3586c03baae400644546
thiisisamazing <34226777+thiisisamazing@users.noreply.github.com>
2018-10-26 08:08:11 -0500

    Update dryfunction.py

--- dryfunction.py
+++ dryfunction.py
@@ -4,3 +4,10 @@ def word(monty):
 for monty in ("spam ","eggs "):
     word(monty)
 #rather than writing a bunch of garbage out, we can just use a for loop. pretty groovy.
+#another one:
+
+def multi(x):
+print (x*3)
+
+for x in (3,4,5,6):
+multi(x)
