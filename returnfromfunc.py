1d7601df09dad2a1f6d90ebbbe2626b133f25919
thiisisamazing <34226777+thiisisamazing@users.noreply.github.com>
2018-10-28 23:10:07 -0500

    Create returnfromfunc.py

---
+++ returnfromfunc.py
@@ -0,0 +1,7 @@
+def addme(x,y):
+    z=x+y
+    return (z)
+    print ("quack quack")
+    #text above will never be printed because program stops running after return is executed
+    
+print (addme(3,9))
